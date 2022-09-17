9b4201260543d68168507795672d15c1a0862ebf
ULADZIMIR MARTYNCHY <83595316+Waldemar-00@users.noreply.github.com>
2022-09-17 22:25:40 +0300

    Add files via upload

---
+++ JSLearning/src/script.js
@@ -0,0 +1,40 @@
+
+// let questions = ['How old are you?', 'What is your name?', 'What is your last name?',  ];
+// let answers = [];
+// 
+// for(let i = 0; i < questions.length; i++) {
+    // answers[i] = prompt(questions[i]);
+// }
+// 
+// document.write(answers);
+
+
+function myFirstApp(name, age) {
+    alert(`Hello, my name is ${name} and this is my first application!`);
+
+    function showSkills() {
+        let skills = ['HTML', 'CSS', 'JAVASCRIPT',];
+        for(let i of skills) {
+            console.log(`I have the skill: ${i}`);
+        }
+    }
+    showSkills();
+
+    function checkAge(age) {
+        if(age >= 18) {
+            console.log(`You have chance to become a developer!`);
+        }else if(age < 18) {
+            console.log(`You have GREAT chance to become a developer!`);
+        }else {
+            console.log(`Who are you?))`);
+            }
+    }
+    checkAge(age);
+
+    function calcPow(num) {
+        return num ** 2;
+    }
+    console.log(calcPow(4));
+    
+}
+myFirstApp('Uladzimir', 40);
